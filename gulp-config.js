ea6df7a04f9e8e1a61ab456ed07e235077f6d424
jc_adavydenko <zabell2814@gmail.com>
2018-02-28 23:41:12 +0200

    init repo

---
+++ gulp-config.js
@@ -0,0 +1,86 @@
+module.exports = {
+  folder: {
+    tasks: 'tasks',
+    src: 'src',
+    build: 'assets',
+    prod: 'production',
+    pug: 'src/templates'
+  },
+  file: {
+    mainHtml: 'index.html',
+    mainJs: 'app.js',
+    vendorJs: 'vendor.js',
+    vendorJsMin: 'vendor.min.js',
+    mainScss: 'style.scss',
+    mainScssMin: 'style.min.css',
+    vendorScss: 'vendor.scss',
+    vendorScssMin: 'vendor.min.css',
+  },
+  task: {
+    templates: 'templates',
+    jsHint: 'js-hint',
+    buildCustomJs: 'build-custom-js',
+    buildJsVendors: 'build-js-vendors',
+    buildSass: 'build-sass',
+    buildSassFiles: 'compile-sass-files',
+    buildSassProd: 'build-sass-production',
+    buildStylesVendors: 'build-styles-vendors',
+    imageMin: 'image-min',
+    imageClean: 'image-clean',
+    cleanProd: 'clean-production',
+    cleanBuild: 'clean-build',
+    copyFolders: 'copy-folders',
+    copyFoldersProduction: 'copy-folders-production',
+    browserSync: 'browser-sync-server',
+    watch: 'watch',
+  },
+  autoprefixer: {
+    versions: 'last 4 versions'
+  },
+  imageExtensions: 'jpg|jpeg|png|svg|gif|ico|tiff',
+  getPathesForSassCompiling: function () {
+    return {
+      files: [],
+      isGcmq: false
+     };
+  },
+  getPathesToCopyForProduction: function() {
+    return [
+      './**/*',
+      `!{${this.folder.src},${this.folder.src}/**}`,
+      '!{bower,bower/**}',
+      '!{node_modules,node_modules/**}',
+      `!${this.folder.build}/css/**.map`,
+      `!${this.folder.build}/images/info.txt`,
+      '!.bowerrc',
+      '!bower.json',
+      '!.gitignore',
+      '!gulpfile.js',
+      '!LICENSE',
+      '!package.json',
+      `!${this.folder.prod}`,
+      '!README.md',
+      '!readme.txt',
+      '!CONTRIBUTING.md',
+      '!gulp-config.js',
+      '!package-lock.json',
+      '!{tasks,tasks/**}',
+      '!{.git,.git/**}',
+      '!{.history,.history/**}',
+      '!.gitattributes',
+      '!.travis.yml',
+      '!.eslintrc',
+      '!{tasks,tasks/**}'
+    ];
+  },
+  getPathesToCopy: function() {
+    return [
+      `./${this.folder.src}/**`,
+      `!{${this.folder.src}/images,${this.folder.src}/images/**}`,
+      `!{${this.folder.src}/js,${this.folder.src}/js/**}`,
+      `!{${this.folder.src}/scss,${this.folder.src}/scss/**}`,
+      `!{${this.folder.src}/vendor_entries,${this.folder.src}/vendor_entries/**}`,
+      `!{${this.folder.src}/templates,${this.folder.src}/templates/**}`
+    ];
+  }
+};
---
+++ tasks/watch.js
@@ -0,0 +1,34 @@
+/**
+ * Watch for file changes
+ */
+'use strict';
+
+const gulp = require('gulp');
+
+module.exports = function (options) {
+  return () => {
+    gulp.watch(`./${options.src}/js/**/*`, gulp.series(options.tasks.buildCustomJs, options.tasks.jsHint));
+
+    gulp.watch(`${options.pug}/**/*.pug`, gulp.series(options.tasks.templates))
+      .on('all', (path) => {
+        global.emittyChangedFile = path;
+      });
+
+    gulp.watch(`./${options.src}/scss/**/*`, gulp.series(options.tasks.buildSass));
+
+    const imagesWatcher = gulp.watch(`./${options.src}/images/**/*.+(${options.imageExtensions})`);
+
+    imagesWatcher
+      .on('unlink', (path) => {
+        options.deleteFile({
+          path,
+          event: 'unlink'
+        }, options.src, options.dest);
+      })
+      .on('add', gulp.series(options.tasks.imageMin));
+
+    gulp.watch([`./${options.dest}/**/*`, './*.html'])
+      .on('change', options.browserSync.reload);
+  };
+
+};
