b7f89026a11f151b1e23bb4e3a406f92e023158a
Zabellfour <zabell2814@gmail.com>
2018-03-04 17:18:28 +0200

    resp slider

--- src/js/modules/sliders.js
+++ src/js/modules/sliders.js
@@ -2,7 +2,22 @@ var sliders = {
     accSlider: function() {
         $('.accelerators-holder').owlCarousel({
           items:4,
-          mouseDrag: false
+          mouseDrag: false,
+           responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+      768:{
+            items:2,
+            nav:false
+        },
+       1024:{
+            items:4,
+            nav:true,
+            loop:false
+        }
+    }
         });
         $(".circle-text").each(function(index, el) {
           new CircleType(el).radius(134);
